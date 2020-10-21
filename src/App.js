fe9b127d66b0918a901abbd78781f676daf72273
KulchytskyiOleh <olegkakb@gmail.com>
2020-10-21 13:31:28 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
-
+/*-e-*/
 function App(props) {
   return (
     <div className="App">
-      <Header/>
-      <Content todosData={props.todosData} addTodo={props.addTodo} />
+      <Header />
+      <Content
+        className="content"
+        todosData={props.todosData}
+        addTodo={props.addTodo}
+      />
       <Footer contacts={props.contacts} />
     </div>
   );

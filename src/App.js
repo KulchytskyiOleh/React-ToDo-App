25580f9545455937d97bf82c0bca46e51b2a7a11
KulchytskyiOleh <olegkakb@gmail.com>
2020-10-21 12:50:04 +0300

    fd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
-
+/**/ 
 function App(props) {
   return (
     <div className="App">

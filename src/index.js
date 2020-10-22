8c0a8ececc128a219118b97aed8aa0cd87c2a3b0
KulchytskyiOleh <olegkakb@gmail.com>
2020-10-22 12:13:37 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import todosData from "./components/Data/todosData";
-import { addTodo } from "./components/Data/todosData";
-import { contacts } from "./components/Data/todosData";
+import todosData, { addTodo, contacts } from "./components/Data/todosData";
 
 ReactDOM.render(
   <App contacts={contacts} todosData={todosData} addTodo={addTodo} />,
